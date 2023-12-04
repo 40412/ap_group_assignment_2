2efe4ccbd7aa3199d1b1449ac2ca7e18fb9269a7
Jasmin Fränti <jasmin.franti@edu.turkuamk.fi>
2023-12-04 15:31:42 +0200

    added some html files to test urls and views. + 1 test template

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -1,8 +1,13 @@
 # Recipe app urls
 from django.urls import path
 from . import views
+
 app_name = 'recipe_app'
+
 urlpatterns = [
     # Main page, using same naming as in the book
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('recipes/', views.recipes, name='recipes'),
+    path('recipe/<int:recipe_id>', views.recipe_detail, name='recipe_detail'),
+    path('testing/', views.testview, name='test'),
 ]
--- recipe_app/views.py
+++ recipe_app/views.py
@@ -3,15 +3,15 @@ from django.contrib.auth.decorators import login_required
 from .models import Recipe, Ingredients, Favorites, Rating
 # Create your views here.
 
-'''# Main page
+# Main page
 def index(request):
-    return render(request, 'recipe_app/html for main page')'''
+    return render(request, 'recipe_app/index.html')
 
 # Display all recipes
 def recipes(request): # + sort type
     recipes = Recipe.objects.order_by('date_added') #sort by popularity?
     context = {'recipes':recipes}
-    return render(request, 'recipe_app/html for recipes page', context)
+    return render(request, 'recipe_app/all_recipes.html', context)
 
 # Detailed recipe view
 def recipe_detail(request, recipe_id):
@@ -19,9 +19,11 @@ def recipe_detail(request, recipe_id):
     ingredients = recipe.Ingredients.get()
     ratings = recipe.ratings.all()
     context = {'recipe':recipe, 'ingredients':ingredients, 'ratings':ratings}
-    return render(request, 'recipe detail html', context)
+    return render(request, 'recipe_app/recipe_detail.html', context)
 # Add recipe view
 # Edit recipe view
 # Add rating view
 # Add to favorites view
 # User favorite recipes
+def testview(request):
+    return render(request, 'recipe_app/test.html')
