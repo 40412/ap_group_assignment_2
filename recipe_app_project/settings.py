094a78cbb099a032cc9a264eebe750587399d07b
Jasmin Fränti <jasmin.franti@edu.turkuamk.fi>
2023-12-06 13:45:44 +0200

    Added account creation,  log in and logout

--- recipe_app_project/settings.py
+++ recipe_app_project/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #additional apps
     'recipe_app',
+    'accounts',
     #default apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -130,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'recipe_app:index'
+LOGOUT_REDIRECT_URL = 'recipe_app:index'
